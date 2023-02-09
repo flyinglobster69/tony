f5f77417f85b7691c0e598a82058d07307dfdfa3
Arthur Jin <arthurjin01@outlook.com>
2023-02-08 22:31:59 -0800

    Alpha 0.9.2 - Switched to POS-JS, can only remember the last word in a sentence

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@
 const Discord = require('discord.js')
 const fs = require('fs')
 const { exitCode } = require('process')
+const pos = require('pos')
 
 // Connect Config file 
 const config = require('./config.json')
@@ -32,8 +33,7 @@ const config = require('./config.json')
 // Connect Client
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, 
     Discord.Intents.FLAGS.GUILD_MEMBERS, 
-    Discord.Intents.FLAGS.GUILD_MESSAGES
-]}
+    Discord.Intents.FLAGS.GUILD_MESSAGES]}
 ) // Specify intents
 
 // Gets called when TonyAI is successfully logged on and connected
@@ -45,18 +45,193 @@ client.on('ready', () => {
 
 client.on('messageCreate', message => {
     console.log('Message Detected')
-    if (message.author = client.user) {
+    if (message.author == client.user) {
         return // prevent bot from responding to own messages
     }
-    if (message.content.toLowerCase() == 'test') {
+    if (message.content.toLowerCase() == 'test') { // Test command to make sure the bot responds
         console.log('Command Activated')
         message.channel.send('Passed')
     }
+
     
     else {
-        null //
+        // Tag parts of speech using POS-JS
+
+        // Input message contents into lex, then tag each word with a part of speech
+        var words = new pos.Lexer().lex(message.content.toLowerCase()) 
+        var tagger = new pos.Tagger()
+        var taggedWords = tagger.tag(words) 
+
+        let len = message.content.split(' ').length
+
+        // For each tagged word, read the CSV for the tag, then add the word to the CSV if word isn't there already
+        for (i = 0; i < len; i++) {
+            var taggedWord = taggedWords[i]
+            var word = taggedWord[0]
+            var tag = taggedWord[1]
+            console.log(`${word} - ${tag}`)
+
+            // Remember words that have been tagged
+            fs.open(`./engcsv/${tag}.csv`, 'r+', function(error, fd) {
+                console.log('Open CSV')
+                console.log(word)
+
+                if (error) {
+                    // Create CSV if file not found
+                    fs.writeFile(`./engcsv/${tag}.csv`, word, 'utf8', function(error, data) { // start user with 1 pog
+                        null
+                    })
+                    console.log(`${tag} CSV created.`)
+                }
+                else {
+                    // Read data from CSV and then add new word to it if needed
+                    fs.readFile(`./engcsv/${tag}.csv`, 'utf8', function(error, data) {
+                        if (error) {
+                            null
+                            // No Read error should occur if file exists
+                        }
+                        else {
+                            console.log('Reading CSV')
+
+                            // Split existing data into a list
+                            let contents = data.split(',')
+                            console.log('Evalating existing word list')
+                            console.log(contents)
+
+                            let wc = 0
+                            let sameword = false
+
+                            // Compare each existing word with the word that was collected
+                            for (wc = 0; wc < contents.length; wc++) {
+                                let existword = contents[wc]
+                                console.log(contents.length)
+
+                                if (existword == word) {
+                                    // If word was found in the CSV, do not add word to CSV
+                                    sameword = true
+                                    console.log('Found same word')
+                                }
+                                else {
+                                    null
+                                    // Leave sameword = false if word not same
+                                }
+                            }
+
+                            // If no same word in CSV, add word to the CSV
+                            if (!sameword) {
+                                console.log(word)
+
+                                // Add word to the list of words already present in the CSV
+                                let finalwordlist = contents.push(word)
+
+                                // Overwrite the previous CSV with the updated list
+                                fs.write(fd, finalwordlist, 0,'utf8', function(error, writtenbytes) {
+                                    console.log('Write to CSV')
+                                }) 
+                            }
+                        }
+                    })
+                }
+            })
+        }
+        
+
+
+        
+        // let wordcount = 0
+        // let nouns = []
+        // let pronouns = ['i', 'you', 'he', 'she', 'him', 'her', 'it', 'them', 'ours']
+        // let verbs = []
+        // let adjectives = []
+        // let adverbs = []
+        // let prepositions = []
+        // let conjunctions = ['and', 'but', 'or', 'so', 'yet', 'with']
+        // let articles = ['a', 'an', 'the']
+        // let interjections = []
+
+        // for (wordcount = 0; wordcount < words.length; wordcount++) {
+        //     let word = words[wordcount]
+        //     console.log(word)
+        //     // Nouns
+        //     // Person, Place, Thing -> Objects
+
+        //     // Pronouns
+        //     // Replaces a Noun
+
+        //     // Verbs
+        //     // Action
+
+        //     // Adjectives
+        //     // Describes a noun; comes before a noun
+
+        //     // Adverbs
+        //     if (word.endsWith('ly')) {
+        //         console.log('Adv detected')
+        //         fs.open('./engcsv/adverbs.csv', 'r+', function(error, fd) {
+        //             console.log('Open Adv CSV')
+        //             adverbs.push(word)
+        //             words.splice(word)
+        //             console.log(words)
+        //             if (error) {
+        //                 null
+        //             }
+        //             else {
+        //                 fs.readFile('./engcsv/adverbs.csv', 'utf8', function(error, data) {
+        //                     if (error) {
+        //                         null
+        //                     }
+        //                     else {
+        //                         console.log('Reading Adv CSV')
+        //                         let contents = data.split(',')
+        //                         console.log(contents)
+        //                         let advcount = 0
+        //                         let sameword = false
+        //                         for (advcount = 0; advcount < contents.length; advcount++) {
+        //                             let existadv = contents[advcount]
+        //                             console.log(contents.length)
+        //                             if (existadv == word) {
+        //                                 sameword = true
+        //                                 console.log('Found same word')
+        //                             }
+        //                             else {
+        //                                 sameword = false
+        //                             }
+        //                         }
+        //                         if (!sameword) {
+        //                             console.log(word)
+        //                             finaladvlist = word.split(' ')
+        //                             finaladvcount = 0
+        //                             for (finaladvcount = 0; finaladvcount < finaladvlist.length; finaladvcount++) {
+        //                                 finaladv = finaladvlist[finaladvcount]
+        //                                 if (finaladv.endsWith('ly')) {
+        //                                     contents.push(finaladv)
+        //                                     let rewrite = contents.toString()
+        //                                     console.log(rewrite)
+        //                                     fs.write(fd, rewrite, 0,'utf8', function(error, writtenbytes) {
+        //                                         console.log('Write to Adv CSV')
+        //                                     }) 
+        //                                 }
+        //                             }
+        //                         }
+        //                     }
+        //                 })
+        //             }
+        //         })
+        //     }
+
+            // Prepositions
+
+            // Conjunctions
+
+            // Articles
+
+            // Interjections 
+            // Catch all that don't fit in previous categories
+
+//         }
     }
 })
+
 
 // Function for machine learning
 // Parts of speech categories
@@ -113,6 +288,8 @@ client.on('messageCreate', message => {
 //     }
 // })
 
+
+// When the bot is missing permissions 
 process.on('unhandledRejection', (reason, promise) => {
     console.log('no perms')
 })
